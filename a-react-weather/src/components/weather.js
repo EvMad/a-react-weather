d86c5ba07ba26d1ecee6c84788e00c005f4a4af0
GitPit <rifflifter@gmail.com>
2022-06-16 13:31:11 -0400

    adjusted sunrise and sunset times to match local

--- a-react-weather/src/components/weather.js
+++ a-react-weather/src/components/weather.js
@@ -7,8 +7,8 @@ const CardExampleCard = ({weatherData}) => (
     <Card.Content>
         <Card.Header className="header">{weatherData.name}</Card.Header>
         <p>Temperature: {weatherData.main.temp}</p>
-        <p>Sunrise: {weatherData.sys.sunrise}</p>
-        <p>Sunset: {weatherData.sys.sunset}</p>
+        <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
         <p>Description: {weatherData.weather[0].description}</p>
     </Card.Content>
   </Card>
