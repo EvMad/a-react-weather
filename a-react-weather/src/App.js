831e3d318fe0ae933a3a9aac45723fae187c4524
GitPit <rifflifter@gmail.com>
2022-06-16 12:49:57 -0400

    created lat and long const

--- a-react-weather/src/App.js
+++ a-react-weather/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 
+const [lat, setLat] = useState([]);
+const [long, setLong] = useState([]);
+
 function App() {
   return (
     <div className="App">
